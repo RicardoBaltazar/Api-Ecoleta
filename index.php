6bf3986cf415d38e0fb44d3731bf8d78b6b20db2
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-26 01:54:58 -0300

    feature - criando menu

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="responsive.css">
+    <title>Professora Ribeiro</title>
+</head>
+
+<body>
+    <main class="home-page">
+        <nav class="menu">
+            <p>Professora Ribeiro</p>
+            <ul>
+                <li><a href="">Página inicial</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Método</a></li>
+                <li><a href="">Recomendações</a></li>
+                <li><a href="">FAQ</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </main>
+</body>
+
+</html>
