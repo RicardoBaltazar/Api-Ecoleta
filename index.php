e844c03bf63f7ef569a4c1af06dd8c31c3e62032
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-26 13:48:42 -0300

    style - adicionando elementos no menu

--- index.php
+++ index.php
@@ -6,26 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="responsive.css">
     <title>Professora Ribeiro</title>
 </head>
 
+
 <body>
-    <main class="home-page">
-        <nav class="menu">
+    <div class="home-page">
+        <header class="menu">
             <p>Professora Ribeiro</p>
-            <ul>
-                <li><a href="">Página inicial</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Método</a></li>
-                <li><a href="">Recomendações</a></li>
-                <li><a href="">FAQ</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
-    </main>
+            <nav>
+                <ul>
+                    <li><a href="">Página inicial</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Método</a></li>
+                    <li><a href="">Recomendações</a></li>
+                    <li><a href="">FAQ</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <h1>asd</h1>
+        </main>
+
+    </div>
+
 </body>
 
 </html>
