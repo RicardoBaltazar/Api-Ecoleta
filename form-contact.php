bec32ade8e500c259c073385296c96a1cd9dbe1c
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-29 20:39:14 -0300

    feature - configurando checkbok e variaveis

--- form-contact.php
+++ form-contact.php
@@ -6,6 +6,28 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
+//$fundamental = $_POST['fundamental'];
+
+if($_POST['fundamental'] == 'on'){
+    $fundamental = 'ensino fundamental';
+} else {
+    //unset($fundamental);
+    $fundamental = '';
+}
+
+if($_POST['medio'] == 'on'){
+    $medio = 'Ensíno Médio';
+} else {
+    //unset($medio);
+    $medio = '';
+}
+
+if($_POST['vestibular'] == 'on'){
+    $vestibular = 'Vestibular';
+} else {
+    $vestibular = '';
+}
+
 echo $name;
 echo '<br>';
 echo $last_name;
@@ -15,6 +37,14 @@ echo '<br>';
 echo $phone;
 echo '<br>';
 echo $message;
+
+echo '<br>';
+echo '<br>';
+echo $fundamental;
+echo '<br>';
+echo $medio;
+echo '<br>';
+echo $vestibular;
 
 
 ?>
