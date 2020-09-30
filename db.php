4886d2c12a04182b1adad8f8edf2051e2a42dda8
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-09-30 14:52:51 -0300

    feature - criando arquivo de conexão com database

--- db.php
+++ db.php
@@ -9,7 +9,15 @@
     
 
         public function connect_mysql(){
-             
+            $connection = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+
+            mysqli_set_charset($connection, 'UTF8');
+
+            if (mysqli_connect_errno()){
+                echo 'Houve um erro ao tentar se conectar com o banco de dados mysql: '.mysqli_connect_error();
+            }
+    
+            return $connection;
         }
 
     }
